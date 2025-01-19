fb03baf8a81dc9cfef0b1d0bca4b135f16495b4b
Peter Grillet <pagrillet@gmail.com>
2025-01-19 20:15:03 +0000

    Initial Commit

---
+++ hustlenscroll/hustlenscrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  hustlenscrollApp.swift
+//  hustlenscroll
+//
+//  Created by Peter Grillet on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct hustlenscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hustlenscrollTests/hustlenscrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  hustlenscrollTests.swift
+//  hustlenscrollTests
+//
+//  Created by Peter Grillet on 19/01/2025.
+//
+
+import Testing
+@testable import hustlenscroll
+
+struct hustlenscrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
